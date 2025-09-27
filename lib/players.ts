22efcb4258e8fccfd1da288766a5164297576b9c
HectorGtz27 <alan.gtz51@gmail.com>
2025-09-26 22:50:44 -0600

    Create, read, update and delete operations for players

---
+++ lib/players.ts
@@ -0,0 +1,34 @@
+import { Player } from "../types/player";
+import { db } from "./firebase";
+import { collection, addDoc, getDocs, updateDoc, doc, deleteDoc} from "firebase/firestore";
+
+const playersCol = collection(db, "players");
+
+
+// Create 
+export async function addPlayer(player: Omit<Player, "id">){
+    return await addDoc(playersCol, player)
+}
+
+
+// Read
+export async function getPlaters(): Promise<Player[]>{
+    const snapshot = await getDocs(playersCol)
+    return snapshot.docs.map((docsSnap)=> ({
+        id: docsSnap.id,
+        ...docsSnap.data()
+    })) as Player[];
+}
+
+// Update
+export async function updatePlayer(id: string, data: Partial<Player>){
+    const playerRef = doc(db, "players", id);
+    return await updateDoc(playerRef, data)
+}
+
+// Delete 
+export async function deletePlater(id: string){
+    const playerRef = doc(db, "players", id);
+    return await deleteDoc(playerRef)
+
+}
